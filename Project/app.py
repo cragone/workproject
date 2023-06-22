9d24d64aaff916590efe5199540a2ddf84e03ad7
cragone <132158708+cragone@users.noreply.github.com>
2023-06-21 21:56:40 -0400

    got data on area page

--- Project/app.py
+++ Project/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from functions.areafunc import pullarea
+from functions.areafunc import pull_area, room_getter
 
 app = Flask(__name__)
 
@@ -14,12 +14,21 @@ def reg_page():
 @app.route("/area", methods=['GET'])
 def processpage():
     process_area_name = request.args.get('process_area_name', None, str)
-    if pullarea(process_area_name) == process_area_name:
+    area = pull_area(process_area_name)
+    if area:
             return render_template("area.html", area_name=process_area_name)
     else:
-            return 'This Area does not exist'
-
-
+            return 'This Area does not exist', 404
+     
+@app.route("/rooms", methods=["GET"])
+def roompage():
+    room_name = request.args.get('process_area_name', None, str)
+    rooms = room_getter(room_name)
+    if rooms:
+        return rooms
+    else:
+        return 'No rooms in this area', 404
+        
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
--- Project/functions/areafunc.py
+++ Project/functions/areafunc.py
@@ -3,7 +3,7 @@ import psycopg2
 
 
 
-def pullarea(process_area_name):
+def pull_area(process_area_name):
     connection = psycopg2.connect(
                     user="postgres",
                     password="postgrespostgres",
@@ -13,7 +13,32 @@ def pullarea(process_area_name):
              )
     cursor = connection.cursor()
     query = "SELECT process_area_name FROM process_area WHERE process_area_name = %s"
-    cursor.execute(query, (process_area_name,))
-    process_area_name = cursor.fetchone()[0]
-    return process_area_name 
+    try:
+        cursor.execute(query, (process_area_name,))
+        process_area_name = cursor.fetchone()[0]
+   
+        return process_area_name
+    except:
+        
+        return None
+
+
+
+
+def room_getter(process_area_name):
+    connection = psycopg2.connect(
+                    user="postgres",
+                    password="postgrespostgres",
+                    host="localhost",
+                    port="5432",
+                    database="workproject"
+             ) 
+    cursor = connection.cursor()
+    query = "SELECT room_name FROM room WHERE process_area_name = %s"
+    try:
+        cursor.execute(query, (process_area_name,))
+   
+        return cursor.fetchall()
+    except:
+        return None
 
