72a578ac0cfa6df5a21fa464c2f75a6cd16ea433
cragone <132158708+cragone@users.noreply.github.com>
2023-06-22 21:31:39 -0400

    new function file for submissions

---
+++ Project/functions/subfunc.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+
+
+def room_note(room_name, room_notes, datetime):
+    connection = psycopg2.connect(
+                    user="postgres",
+                    password="postgrespostgres",
+                    host="localhost",
+                    port="5432",
+                    database="workproject"
+             )
+    cursor = connection.cursor()
+    query = "INSERT INTO notes (room_name, room_notes, datetime) values (%s, %s, %s); COMMIT;"
+
+    try:
+        cursor.execute(query, (room_name, room_notes, datetime))
+        cursor.close()
+        connection.close()
+        return 'good'
+    except:
+        
+        return None
