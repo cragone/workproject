a6dff9554cda8da9d85baf2c9a52ff884efbc788
cragone <132158708+cragone@users.noreply.github.com>
2023-07-10 14:44:19 -0400

    added equip query

--- Project/functions/subfunc.py
+++ Project/functions/subfunc.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 
-#function for room submission info
+#query function for room info submission
 def room_note(room_name, room_notes, datetime):
     connection = psycopg2.connect(
                     user="postgres",
@@ -22,4 +22,26 @@ def room_note(room_name, room_notes, datetime):
         
         return None
     
-#Making function for equipment submission info
+
+
+#query function for equipment infor submission
+def equip_note(room_name, ecn, status, expiry):
+    connection = psycopg2.connect(
+                    user="postgres",
+                    password="postgrespostgres",
+                    host="localhost",
+                    port="5432",
+                    database="workproject"
+             )
+    cursor = connection.cursor()
+    query = "INSERT INTO equipment (room_name, ecn, staus, expiry) values (%s, %s, %s, %s); COMMIT;"
+
+    try:
+        cursor.execute(query, (room_name, ecn, status, expiry))
+        cursor.close()
+        connection.close()
+        return 'good'
+    except:
+        
+        return None
+    
